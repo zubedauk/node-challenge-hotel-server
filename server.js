423c4a53706a83b1a385c14ced333de7ccdc35ba
zubeda <zubedauk@gmail.com>
2020-11-03 02:17:57 +0000

    level 5

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+var moment = require('moment'); 
+var validator = require("email-validator");
 
 const app = express();
 
@@ -14,22 +16,60 @@ app.get("/", function (request, response) {
 });
 //***************Create a new booking */
 app.post("/bookings",function(req,res){
-  const id=bookings.length+1;
-  bookings.push( {
-    "id": id,
-    "title": req.body.title,
-    "firstName": req.body.firstName,
-    "surname": req.body.surname,
-    "email": req.body.email,
-    "roomId": req.body.roomId,
-    "checkInDate": req.body.checkInDate,
-    "checkOutDate": req.body.checkOutDate
-  })
-res.send("addedd successfully")
+  //validator.validate(req.body.email)===false;
+  if(req.body.title===undefined
+    || req.body.firstName===undefined || req.body.surname===undefined || req.body.email===undefined || req.body.roomId===undefined || req.body.checkInDate===undefined || req.body.checkOutDate===undefined ){
+    res.status(400);
+    res.send("fill all fields please")
+  }else if(!validator.validate(req.body.email)){
+    res.send("correct email formate ie test@hotail.com");
+  }
+  else{
+    const id=bookings.length+1;
+    bookings.push( {
+      "id": id,
+      "title": req.body.title,
+      "firstName": req.body.firstName,
+      "surname": req.body.surname,
+      "email": req.body.email,
+      "roomId": req.body.roomId,
+      "checkInDate": req.body.checkInDate,
+      "checkOutDate": req.body.checkOutDate
+    })
+    res.send("addedd successfully")
+  }
+
 })
 //*******************8read all booking
 app.get("/bookings",function(req,res){
   res.json(bookings);
+})
+//**************search by term */
+//***************search by date */
+app.get("/bookings/search",function(req,res){
+
+  //res.send("please put date formate yyyy:mm:dd ie search?date=2019-05-20")
+  //const date = moment(req.body.date)
+  if(req.query.date){
+    const found=bookings.find(function(obj){
+        return obj.checkInDate==req.query.date || obj.checkOutDate==req.query.date;
+    })
+    if(found){
+      res.send(found)
+    }else{
+      res.send("no found search date")
+    }
+  }else{
+    const found=bookings.find(function(obj){
+      return obj.firstName.toLowerCase().includes(req.query.term.toLowerCase()) || obj.surname.toLowerCase().includes(req.query.term.toLowerCase()) || obj.email.toLowerCase().includes(req.query.term.toLowerCase())
+    })
+    if(found){
+      res.json(found)
+    }
+    else{
+      res.send("no term exist")
+    }
+  }
 })
 //**************************Read one booking, specified by an ID */
 app.get("/bookings/:id",function(req,res){
@@ -42,6 +82,7 @@ app.get("/bookings/:id",function(req,res){
   }
   else{
     res.status(404);
+    res.send("not found this id")
   }
  
 })
